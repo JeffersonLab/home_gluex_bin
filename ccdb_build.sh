92169ad86bc512f610d50fc56387076ee2fe18e0
Mark Ito <marki@jlab.org>
2011-09-05 21:11:37 +0000

    prototype
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@8252 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

---
+++ ccdb_build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+cd /scratch/marki
+rm -rf ccdb.old
+if [ -d ccdb ]
+    then
+    mv ccdb ccdb.old
+fi
+svn checkout https://phys12svn.jlab.org/repos/trunk/ccdb
+cd ccdb
+. environment.bash
+scons
+scons
+ccdb_tests -t
+chmod a+x bin/ccdbcmd
+ccdbcmd -i <<EOT
+help
+quit
++
+EOT
+exit
+
