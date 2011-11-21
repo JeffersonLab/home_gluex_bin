bd4ebf62cc7ec68d4811ec3ac261e09a08e1816c
Mark Ito <marki@jlab.org>
2011-11-21 14:20:06 +0000

    ignore warning on hdv_mainframe_Dict.cc
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@8577 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- nightly_report.sh
+++ nightly_report.sh
@@ -5,7 +5,7 @@ rm -f $REPORT_FILE
 echo ================ ERRORS ================ > $REPORT_FILE
 grep -e ' Error ' -e 'Command not found' -e 'error: ' -e 'No such file' $BUILD_DIR/*.log >> $REPORT_FILE
 echo ================ WARNINGS ============== >> $REPORT_FILE
-grep -e ' warning: ' -e ' Warning: ' $BUILD_DIR/*.log >> $REPORT_FILE
+grep -e ' warning: ' -e ' Warning: ' $BUILD_DIR/*.log | grep -v hdv_mainframe_Dict.cc >> $REPORT_FILE
 lines=`wc -l $REPORT_FILE | perl -n -e 'split; print $_[0]'`
 echo "number of lines in report file $REPORT_FILE is $lines"
 if [ $lines -gt 2 ]
