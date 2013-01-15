51420a318ab83ec4e28d792149d45c4496133b2b
Mark Ito <marki@jlab.org>
2013-01-15 21:12:28 +0000

    do dump on disk local to mysql server
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@10252 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- ccdb_mirror.sh
+++ ccdb_mirror.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-dumpfile=/scratch/$USER/ccdb_dump.sql
-if [ -f $dumpfile ]
-    then
-    mv $dumpfile ${dumpfile}.old
-fi
-mysqldump -hhalldweb1 -uccdb_user ccdb > $dumpfile
+#
+dumpfile=/scratch/marki/ccdb_dump.sql
+#
+env -u SSH_AUTH_SOCK ssh -i ~/.ssh/ccdb_dump -lmarki halldweb1
+ls -l $dumpfile
 mysql -hhallddb1 -umarki -pWga\*aba0 ccdb < $dumpfile
