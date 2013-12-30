e1c8703f30fc470d6082c146a1df8f7fc280a78f
Mark Ito <marki@jlab.org>
2013-12-30 17:42:35 +0000

    Make temp file in /tmp
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@12292 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- crondiff.sh
+++ crondiff.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 echo crontab_jlabl1.txt \<\> crondiff -l
-rm -f c.tmp ; crontab -l > c.tmp ; diff -s crontab_jlabl1.txt c.tmp
+tempfile=/tmp/crondiff_$USER.tmp
+rm -f $tempfile
+crontab -l > $tempfile
+diff -s crontab_jlabl1.txt $tempfile
