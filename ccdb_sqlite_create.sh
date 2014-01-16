06bec70f270b1707877a1a78d65c479b5ff6059c
Mark Ito <marki@jlab.org>
2014-01-16 18:58:16 +0000

    Copy just-made version to the web directory. M    ccdb_sqlite_create.sh
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@12388 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- ccdb_sqlite_create.sh
+++ ccdb_sqlite_create.sh
@@ -3,4 +3,5 @@ source /group/halld/Software/scripts/build_scripts/gluex_env_jlab.sh
 sqlite_file=/group/halld/Software/calib/ccdb_sqlite/ccdb_`date +%F`.sqlite
 rm -fv $sqlite_file
 $CCDB_HOME/scripts/mysql2sqlite/mysql2sqlite.sh -hhalldweb1 -uccdb_user ccdb | sqlite3 $sqlite_file
+cp -pv $sqlite_file /group/halld/www/halldweb1/html/dist/ccdb.sqlite
 ls -l /group/halld/Software/calib/ccdb_sqlite
