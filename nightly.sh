1a6898ea9ac525776cd3e339aa40df7f4f02ea95
Mark Ito <marki@jlab.org>
2014-10-12 03:28:22 +0000

    fix build directory, it is the scratch disk now
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@15713 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- nightly.sh
+++ nightly.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 hosts="jlabl1 jlabl3 ifarm1102"
 BUILD_SCRIPTS=/group/halld/Software/scripts/build_scripts
-BUILD_DIR=/group/halld/Software/builds/nightly/`date +%F`
+BUILD_DIR=/u/scratch/gluex/nightly/`date +%F`
 for host in $hosts
 do
     logfile=/u/scratch/gluex/halld_$host.log
