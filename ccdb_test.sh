41959eca830024640315aeeeb52a0f745fe513fc
Mark Ito <marki@jlab.org>
2014-11-03 15:40:00 +0000

    check that ccdb gives the help message
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@16294 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

---
+++ ccdb_test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+source /group/halld/Software/scripts/build_scripts/gluex_env_jlab.sh
+export PATH=/apps/python/PRO/bin:$PATH
+export LD_LIBRARY_PATH=/apps/python/PRO/lib:$LD_LIBRARY_PATH
+ccdb
