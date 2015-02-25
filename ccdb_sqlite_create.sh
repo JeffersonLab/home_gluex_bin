c116de3de94d3afa7f570b58c632a71ab1e34f05
Mark Ito <marki@jlab.org>
2015-02-25 20:26:35 +0000

    check that the sqlite file is big enough
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@17390 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- ccdb_sqlite_create.sh
+++ ccdb_sqlite_create.sh
@@ -2,7 +2,14 @@
 source /group/halld/Software/scripts/build_scripts/gluex_env_jlab.sh
 sqlite_file=/group/halld/Software/calib/ccdb_sqlite/ccdb_`date +%F`.sqlite
 rm -fv $sqlite_file
-$CCDB_HOME/scripts/mysql2sqlite/mysql2sqlite.sh -hhalldweb1 -uccdb_user ccdb | sqlite3 $sqlite_file
-cp -pv $sqlite_file /group/halld/www/halldweb1/html/dist/ccdb.sqlite
+$CCDB_HOME/scripts/mysql2sqlite/mysql2sqlite.sh -hhallddb -uccdb_user ccdb | sqlite3 $sqlite_file
+size=`stat --format=%s $sqlite_file`
+echo size = $size
+if [ $size -gt 100000000 ]
+    then
+    cp -pv $sqlite_file /group/halld/www/halldweb1/html/dist/ccdb.sqlite
+else
+    echo too small to copy
+fi
 echo listing showing access time:
 ls -lu /group/halld/Software/calib/ccdb_sqlite
