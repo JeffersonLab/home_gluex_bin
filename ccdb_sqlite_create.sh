675acd24ea37b3d879cd0a256fdbe1ef00438e26
Mark Ito <marki@jlab.org>
2015-02-25 20:39:48 +0000

    tested size check
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@17391 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- ccdb_sqlite_create.sh
+++ ccdb_sqlite_create.sh
@@ -7,9 +7,13 @@ size=`stat --format=%s $sqlite_file`
 echo size = $size
 if [ $size -gt 100000000 ]
     then
-    cp -pv $sqlite_file /group/halld/www/halldweb1/html/dist/ccdb.sqlite
+    cp -pv $sqlite_file /group/halld/www/halldweb/html/dist/ccdb.sqlite.tmp
+    pushd /group/halld/www/halldweb/html/dist
+    mv -v ccdb.sqlite.tmp ccdb.sqlite
 else
-    echo too small to copy
+    echo error: too small to copy
 fi
 echo listing showing access time:
 ls -lu /group/halld/Software/calib/ccdb_sqlite
+echo listing in dist directory showing modified time:
+ls -l /group/halld/www/halldweb/html/dist/ccdb*.sqlite
