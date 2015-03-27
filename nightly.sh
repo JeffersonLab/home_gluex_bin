73cf350c55c9a912d36ae9d19f3b6e22cbb714e7
Mark Ito <marki@jlab.org>
2015-03-27 17:43:29 +0000

    Use ifarm1101, not ifarm1102. That node cannot be accessed anymore
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@17833 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- nightly.sh
+++ nightly.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-hosts="jlabl1 jlabl3 ifarm1102 ifarm1401"
+hosts="jlabl1 jlabl3 ifarm1101 ifarm1401 lorentz"
 BUILD_SCRIPTS=/group/halld/Software/scripts/build_scripts
 BUILD_DIR=/u/scratch/gluex/nightly/`date +%F`
 for host in $hosts
