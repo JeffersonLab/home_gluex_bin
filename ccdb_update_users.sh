7a21957b58d0950b276bdff36962a2de34345a97
Mark Ito <marki@jlab.org>
2015-08-25 12:13:20 +0000

    use build_scripts from its new location use the generic CCDB in favor of one from /home/gluex
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@19261 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- ccdb_update_users.sh
+++ ccdb_update_users.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-export CCDB_HOME=/home/gluex/p12/ccdb
-source /group/halld/Software/scripts/build_scripts/gluex_env_jlab.sh
+source /group/halld/Software/build_scripts/gluex_env_jlab.sh
 perl $CCDB_HOME/scripts/users_create/group_parse.pl \
     | /apps/python/python-2.7.1/bin/python \
     $CCDB_HOME/scripts/users_create/users_create.py \
