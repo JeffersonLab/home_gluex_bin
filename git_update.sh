e408f304603833f0a956eb4fb2f871037a7d31e1
Mark Ito <marki@jlab.org>
2015-09-04 17:56:32 +0000

    Script to update the local versions of repositories hosted at GitHub.
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@19317 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

---
+++ git_update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+list_dir=/group/halld/Software/scripts/simple_email_list/lists/git_update
+message="$list_dir/message.txt"
+rm -fv $message
+touch $message
+for repo in sim-recon hdds hdpm build_scripts git_test
+do
+    echo $repo >> $message
+    cd /group/halld/Repositories/$repo
+    git fetch >> $message
+    git pull >> $message
+done
+cd $list_dir
+../../scripts/simple_email_list.pl
+
