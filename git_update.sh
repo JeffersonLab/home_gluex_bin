89c2f7e67c331a44380c3255598e18103c47880e
Mark Ito <marki@jlab.org>
2015-10-15 19:31:54 +0000

    add gluex_install
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@19515 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- git_update.sh
+++ git_update.sh
@@ -3,12 +3,11 @@ list_dir=/group/halld/Software/scripts/simple_email_list/lists/git_update
 message="$list_dir/message.txt"
 rm -fv $message
 touch $message
-for repo in sim-recon hdds hdpm build_scripts git_test
+for repo in sim-recon hdds hdpm build_scripts git_test gluex_install
 do
     echo ----- $repo ----- >> $message
     cd /group/halld/Repositories/$repo
-    git fetch >> $message
-    git pull >> $message
+    git pull >> $message 2>&1
 done
 cd $list_dir
 ../../scripts/simple_email_list.pl
