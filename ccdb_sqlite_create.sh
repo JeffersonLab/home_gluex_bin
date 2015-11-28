692aa55be37d3c9553692fa01fedbba9b4ba7704
Mark Ito <marki@jlab.org>
2015-11-28 01:06:12 +0000

    reduce printout
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@19722 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- ccdb_sqlite_create.sh
+++ ccdb_sqlite_create.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 source /group/halld/Software/build_scripts/gluex_env_jlab.sh
 sqlite_file=/group/halld/Software/calib/ccdb_sqlite/ccdb_`date +%F`.sqlite
-rm -fv $sqlite_file
+rm -f $sqlite_file
 $CCDB_HOME/scripts/mysql2sqlite/mysql2sqlite.sh -hhallddb -uccdb_user ccdb | sqlite3 $sqlite_file
 size=`stat --format=%s $sqlite_file`
 echo size = $size
 if [ $size -gt 100000000 ]
     then
-    cp -pv $sqlite_file /group/halld/www/halldweb/html/dist/ccdb.sqlite.tmp
+    cp -p $sqlite_file /group/halld/www/halldweb/html/dist/ccdb.sqlite.tmp
     pushd /group/halld/www/halldweb/html/dist
-    mv -v ccdb.sqlite.tmp ccdb.sqlite
+    mv ccdb.sqlite.tmp ccdb.sqlite
 else
     echo error: too small to copy
 fi
-echo listing showing access time:
-ls -lu /group/halld/Software/calib/ccdb_sqlite
-echo listing in dist directory showing modified time:
-ls -l /group/halld/www/halldweb/html/dist/ccdb*.sqlite
