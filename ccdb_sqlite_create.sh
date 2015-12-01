8433cbae6bd8586588137d69fa6d1a2cefe9d755
Mark Ito <marki@jlab.org>
2015-12-01 20:46:58 +0000

    run silently
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@19736 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- ccdb_sqlite_create.sh
+++ ccdb_sqlite_create.sh
@@ -2,13 +2,13 @@
 source /group/halld/Software/build_scripts/gluex_env_jlab.sh
 sqlite_file=/group/halld/Software/calib/ccdb_sqlite/ccdb_`date +%F`.sqlite
 rm -f $sqlite_file
-$CCDB_HOME/scripts/mysql2sqlite/mysql2sqlite.sh -hhallddb -uccdb_user ccdb | sqlite3 $sqlite_file
+$CCDB_HOME/scripts/mysql2sqlite/mysql2sqlite.sh -hhallddb -uccdb_user ccdb | sqlite3 $sqlite_file | grep -v memory
 size=`stat --format=%s $sqlite_file`
-echo size = $size
+#echo size = $size
 if [ $size -gt 100000000 ]
     then
     cp -p $sqlite_file /group/halld/www/halldweb/html/dist/ccdb.sqlite.tmp
-    pushd /group/halld/www/halldweb/html/dist
+    cd /group/halld/www/halldweb/html/dist
     mv ccdb.sqlite.tmp ccdb.sqlite
 else
     echo error: too small to copy
