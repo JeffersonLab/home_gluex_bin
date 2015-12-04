a9ecf569ab456afd8d53a6e0a9a651f81743c3d6
Mark Ito <marki@jlab.org>
2015-12-04 15:11:58 +0000

    run silently
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@19754 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- delete_old_nightly.sh
+++ delete_old_nightly.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-echo delete old nightly builds
+#echo delete old nightly builds
 find /u/scratch/gluex/nightly -maxdepth 1 -mindepth 1 -type d | \
-    perl -n -e 'chomp; $dir = $_; $count = `find $dir -type f | head | wc -l`; chomp $count; if (! $count) {print "removing directory $dir\n"; system "rm -rf $dir\n"}'
+#    perl -n -e 'chomp; $dir = $_; $count = `find $dir -type f | head | wc -l`; chomp $count; if (! $count) {print "removing directory $dir\n"; system "rm -rf $dir\n"}'
+    perl -n -e 'chomp; $dir = $_; $count = `find $dir -type f | head | wc -l`; chomp $count; if (! $count) {system "rm -rf $dir\n"}'
