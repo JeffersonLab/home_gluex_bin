ca44d3194aacf7544a8d59402556edcfe055c59e
Mark Ito <marki@jlab.org>
2016-01-12 12:57:38 +0000

    reduce output to stdout
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@19950 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- ccdb_changes.sh
+++ ccdb_changes.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 tempfile=/tmp/ccdb_changes.txt
 listdir=/group/halld/Software/scripts/simple_email_list
-rm -fv $tempfile
+rm -f $tempfile
 mysql -t -hhallddb -uccdb_user ccdb -e \
 "select assignments.created, variations.name as variation, directories.name as directory, typeTables.name as type, users.name as author, runMin, runMax, substring(vault, 1, 32) as data, assignments.comment from assignments, constantSets, variations, runRanges, users, typeTables, directories where constantSetId = constantSets.id AND variationId = variations.id and runRanges.id = runRangeId and assignments.authorId = users.id and constantTypeId = typeTables.id and directoryId = directories.id having TIMESTAMPDIFF(HOUR,assignments.created, CURDATE()) < 24 order by assignments.created;" > $tempfile
 cd $listdir/lists/ccdb_changes
-cp -v $tempfile message.txt
-$listdir/scripts/simple_email_list.pl
+cp $tempfile message.txt
+$listdir/scripts/simple_email_list.pl | grep -v "At revision"
