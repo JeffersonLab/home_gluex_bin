36ba1ed2818f0eb9a2a6a046ef20fc26bf5e813e
Mark Ito <marki@jlab.org>
2016-06-17 13:51:34 +0000

    move to CCDB-specific set-up since general set-up does not work on jlabl1 anymore
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@20930 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- ccdb_update_users.sh
+++ ccdb_update_users.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-source /group/halld/Software/build_scripts/gluex_env_jlab.sh
+export BUILD_SCRIPTS=/group/halld/Software/build_scripts
+export BMS_OSNAME=`$BUILD_SCRIPTS/osrelease.pl`
+export GLUEX_TOP=/group/halld/Software/builds/$BMS_OSNAME
+eval `/group/halld/Software/build_scripts/version.pl -sbash /group/halld/www/halldweb/html/dist/version_jlab.xml`
+source $CCDB_HOME/environment.bash
 perl $CCDB_HOME/scripts/users_create/group_parse.pl \
     | /apps/python/python-2.7.1/bin/python \
     $CCDB_HOME/scripts/users_create/users_create.py \
