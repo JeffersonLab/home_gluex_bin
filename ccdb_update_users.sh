78eff87d45867d95a53be46a71a718dfb56e3c88
Mark Ito <marki@jlab.org>
2016-11-15 14:10:44 +0000

    update to use standard jlab environment set-up
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@21587 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- ccdb_update_users.sh
+++ ccdb_update_users.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-export BUILD_SCRIPTS=/group/halld/Software/build_scripts
-export BMS_OSNAME=`$BUILD_SCRIPTS/osrelease.pl`
-export GLUEX_TOP=/group/halld/Software/builds/$BMS_OSNAME
-eval `/group/halld/Software/build_scripts/version.pl -sbash /group/halld/www/halldweb/html/dist/version_jlab.xml`
-source $CCDB_HOME/environment.bash
+source /group/halld/Software/build_scripts/gluex_env_jlab.sh
 perl $CCDB_HOME/scripts/users_create/group_parse.pl \
-    | /apps/python/python-2.7.1/bin/python \
-    $CCDB_HOME/scripts/users_create/users_create.py \
+    | python $CCDB_HOME/scripts/users_create/users_create.py \
     --recreate mysql://ccdb_user@hallddb.jlab.org/ccdb
