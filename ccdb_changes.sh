7ea6d163934c3c3d521e01a26b030384f05268c7
Mark Ito <marki@jlab.org>
2017-01-03 18:19:17 +0000

    use new output scheme for simple email list
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@21764 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- ccdb_changes.sh
+++ ccdb_changes.sh
@@ -6,4 +6,4 @@ mysql -t -hhallddb -uccdb_user ccdb -e \
 "select assignments.created, variations.name as variation, directories.name as directory, typeTables.name as type, users.name as author, runMin, runMax, substring(vault, 1, 32) as data, assignments.comment from assignments, constantSets, variations, runRanges, users, typeTables, directories where constantSetId = constantSets.id AND variationId = variations.id and runRanges.id = runRangeId and assignments.authorId = users.id and constantTypeId = typeTables.id and directoryId = directories.id having TIMESTAMPDIFF(HOUR,assignments.created, CURDATE()) < 24 order by assignments.created;" > $tempfile
 cd $listdir/lists/ccdb_changes
 cp $tempfile message.txt
-$listdir/scripts/simple_email_list.pl | grep -v "At revision"
+$listdir/scripts/simple_email_list.pl > /dev/null
