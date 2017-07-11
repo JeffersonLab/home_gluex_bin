f93c44fd59fd222d03d39eb5d2fb776356188627
Mark Ito <marki@jlab.org>
2017-07-11 01:09:28 +0000

    add a word to the output message
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@22409 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- delete_old_nightly.sh
+++ delete_old_nightly.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo delete old nightly builds
 find /u/scratch/gluex/nightly -maxdepth 1 -mindepth 1 -type d | \
-    perl -n -e 'chomp; $dir = $_; $count = `find $dir -type f | wc -l`; chomp $count; print "count for $dir is $count\n"; if (! $count) {print "removing directory $dir\n"; system "rm -rf $dir\n"}'
+    perl -n -e 'chomp; $dir = $_; $count = `find $dir -type f | wc -l`; chomp $count; print "file count for $dir is $count\n"; if (! $count) {print "removing directory $dir\n"; system "rm -rf $dir\n"}'
