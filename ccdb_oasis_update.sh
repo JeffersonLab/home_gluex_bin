1509b90ea24e4544d93ec9667c265d1548124e05
Mark Ito <marki@jlab.org>
2018-01-30 15:00:54 +0000

    changes
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@22961 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- ccdb_oasis_update.sh
+++ ccdb_oasis_update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-if voms-proxy-info ; then
+if voms-proxy-info --exists ; then
     echo there is a proxy
 else
     echo no proxy, need to generate one
-    test.tcl
+    /home/gluex/bin/test.tcl
 fi
-gsiscp -o GSSAPIDelegateCredentials=yes /group/halld/www/halldweb/html/dist/ccdb.sqlite oasis-login.opensciencegrid.org:/stage/oasis/gluex/group/halld/www/halldweb/html/dist/
+gsiscp -o GSSAPIDelegateCredentials=yes /group/halld/www/halldweb/html/dist/ccdb.sqlite oasis-login.opensciencegrid.org:/stage/oasis/gluex/group/halld/www/halldweb/html/dist/ccdb.sqlite.new
+gsissh -o GSSAPIDelegateCredentials=yes oasis-login.opensciencegrid.org mv -v /stage/oasis/gluex/group/halld/www/halldweb/html/dist/ccdb.sqlite.new /stage/oasis/gluex/group/halld/www/halldweb/html/dist/ccdb.sqlite
+gsissh -o GSSAPIDelegateCredentials=yes oasis-login.opensciencegrid.org osg-oasis-update
