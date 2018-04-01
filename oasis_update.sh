3fcd6f2e7a1c948ec34d51da0a4326a870f002b6
Mark Ito <marki@jlab.org>
2018-04-01 23:29:35 +0000

    don't print the whole command, just print a comment
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@23142 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- oasis_update.sh
+++ oasis_update.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+rsync_comment[0]='dist directory'
 rsync_command[0]='rsync -ruvtl --delete --stats -e '\''ssh -i ~/.ssh/oasis_update_rsa'\'' /group/halld/www/halldweb/html/dist/version*.xml /group/halld/www/halldweb/html/dist/*.sqlite ouser.gluex@oasis-login.opensciencegrid.org:/home/login/ouser.gluex/stage/group/halld/www/halldweb/html/dist/'
+rsync_comment[1]="resources directory"
 rsync_command[1]='rsync -ruvtl --delete --stats -e '\''ssh -i ~/.ssh/oasis_update_rsa'\'' /group/halld/www/halldweb/html/resources/ ouser.gluex@oasis-login.opensciencegrid.org:/home/login/ouser.gluex/stage/group/halld/www/halldweb/html/resources'
+rsync_comment[2]="build_scripts directory"
 rsync_command[2]='rsync -ruvtl --delete --stats -e '\''ssh -i ~/.ssh/oasis_update_rsa'\'' /group/halld/Software/build_scripts/ ouser.gluex@oasis-login.opensciencegrid.org:/home/login/ouser.gluex/stage/group/halld/Software/build_scripts/'
+rsync_comment[3]="container build directory"
 rsync_command[3]='rsync -ruvtl --delete --delete-excluded --exclude-from=/home/gluex/bin/oasis_exclude_build.txt --stats -e '\''ssh -i ~/.ssh/oasis_update_rsa'\'' /group/halld/Software/builds/Linux_CentOS7-x86_64-gcc4.8.5-cntr/ ouser.gluex@oasis-login.opensciencegrid.org:/home/login/ouser.gluex/stage/group/halld/Software/builds/Linux_CentOS7-x86_64-gcc4.8.5-cntr/'
 rsync_stdout=/tmp/rsync_stdout.txt
 rsync_script=/tmp/rsync_script.sh
 files_transferred=false
-for this_command in "${rsync_command[@]}";
-do
-  echo this_command = $this_command    
+for i in "${!rsync_command[@]}";
+  do
+  this_comment=${rsync_comment[$i]}
+  this_command=${rsync_command[$i]}
+  echo $i $this_comment
+#  echo $i $this_command
   rm -f $rsync_stdout $rsync_script
   echo '#!/bin/bash' > $rsync_script
   echo $this_command >> $rsync_script
