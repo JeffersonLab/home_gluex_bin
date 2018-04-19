bc06d1a46ec26adec4acbb7b9a626150dfe2c4d9
Mark Ito <marki@jlab.org>
2018-04-19 08:56:58 +0000

    use /u/scratch rather than /scratch
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@23193 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- tar_command_maker.sh
+++ tar_command_maker.sh
@@ -5,7 +5,7 @@ current_dir=`pwd`
 rm -f tar_command.txt
 echo pushd /group/halld/Software/builds > $current_dir/tar_command.txt
 pushd /group/halld/Software/builds
-find . -maxdepth 3 -type d -name $version_label | awk -F/ '{print "tar zcvf /scratch/$USER/"$4"-"$2".tar.gz "$0}' >> $current_dir/tar_command.txt
+find . -maxdepth 3 -type d -name $version_label | awk -F/ '{print "tar zcvf /u/scratch/$USER/"$4"-"$2".tar.gz "$0}' >> $current_dir/tar_command.txt
 popd
 echo tar_command.txt:
 cat tar_command.txt
