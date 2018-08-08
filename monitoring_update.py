9be306dfda22035468d82c0f90c59ee1423c700f
Thomas Britton <tbritton@jlab.org>
2018-08-08 13:12:51 +0000

    updated links to utilize new plot browser
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@23286 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- monitoring_update.py
+++ monitoring_update.py
@@ -19,7 +19,7 @@ def main():
     # date and time for 24 hours previous
     beginTime = datetime.datetime.now() - datetime.timedelta(days=1) #days=1
     beginRun = 0
-    CurrentPeriod = "RunPeriod-2018-01"#"RunPeriod-2017-01"
+    CurrentPeriod = "RunPeriod-2018-08"#"RunPeriod-2017-01"
     #print beginTime
     # get first and last runs for the last 24 hours
     query = "event_count>100000 and @is_2018production" # and time > %s" % beginTime
@@ -50,16 +50,16 @@ def main():
     hist_names = [["CDC_occupancy","CDC"],["FDC_occupancy","FDC"],["FCAL_occupancy","FCAL"],["BCAL_occupancy","BCAL"],["PS_occupancy","PS"],["RF_TPOL_occupancy","RF & TPOL"],["ST_occupancy","ST"],["TAGGER_occupancy","TAGGER"],["TOF_occupancy","TOF"],["DigiHits_occupancy","Hit Multiplicity"]]
 
     for hist in hist_names:
-        message_file.write( "%s: https://halldweb.jlab.org/cgi-bin/data_monitoring/monitoring/plotBrowser.py?run1=%d&run2=%d&plot=%s&ver=rawdata_ver00&period=%s&rcdb_query=%s \n\n" % (hist[1], beginRun, endRun, hist[0], CurrentPeriod, queryshow) )
+        message_file.write( "%s: https://halldweb.jlab.org/data_monitoring/Plot_Browser.html?minRunNum=%d&maxRunNum=%d&RunPeriod=%s&Version=rawdata_ver00&Plot=%s&rcdb_query=%s \n\n" % (hist[1], beginRun, endRun, CurrentPeriod, hist[0], query) )
 
     message_file.write( "\n ===================================================================== \n" )
     message_file.write( "\n \n High Level Online Macros: \n" )
     
     # set list of high_level titles and names in this list
-    high_level_online_names = [["Beam","Beam Properties"],["Kinematics","Recon. Kinematics"],["NumHighLevelObjects","Recon. Num. High-Level Objects"],["PID","Recon. PID"],["Trigger","Trigger (Online)"],["Vertex","Recon. Vertex"] ]
+    high_level_online_names = [["Beam","HistMacro_Beam"],["Kinematics","HistMacro_Kinematics"],["NumHighLevelObjects","HistMacro_NumHighLevelObjects"],["PID","HistMacro_PID"],["Trigger","HistMacro_Trigger"],["Vertex","HistMacro_Vertex"] ]
 
     for hist in high_level_online_names:
-        message_file.write( "%s: https://halldweb.jlab.org/cgi-bin/data_monitoring/monitoring/plotBrowser.py?run1=%d&run2=%d&plot=HistMacro_%s&ver=rawdata_ver00&period=%s&rcdb_query=%s \n\n" % (hist[1], beginRun, endRun, hist[0], CurrentPeriod, queryshow) )
+        message_file.write( "%s: https://halldweb.jlab.org/data_monitoring/Plot_Browser.html?minRunNum=%d&maxRunNum=%d&RunPeriod=%s&Version=rawdata_ver00&Plot=%s&rcdb_query=%s \n\n" % (hist[0], beginRun, endRun, CurrentPeriod, hist[1], query) )
 
     # set list of high_level titles and names in this list
    # high_level_names = [["Beam","Beam"],["Vertex","Vertex"],["Trigger","Trigger"],["NumHighLevelObjects","# HL Obj"],["PID","PID"],["Kinematics","Kinematics"]]
