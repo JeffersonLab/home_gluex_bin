9fb5088a749bf797581c246b62eed43c1fd0148c
Mark Ito <marki@jlab.org>
2019-04-05 17:26:50 +0000

    remove crontab on jlabl1, update monitoring_update.py
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@23933 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- monitoring_update.py
+++ monitoring_update.py
@@ -2,9 +2,11 @@
 
 import datetime
 import os
-os.environ["RCDB_HOME"] = "/group/halld/www/halldweb/html/rcdb_home"
+#os.environ["RCDB_HOME"] = "/group/halld/www/halldweb/html/rcdb_home"
+os.environ["RCDB_HOME"] = "/u/home/gluex/rcdb/"
 import sys
-sys.path.append("/group/halld/www/halldweb/html/rcdb_home/python")
+#sys.path.append("/group/halld/www/halldweb/html/rcdb_home/python")
+sys.path.append("/u/home/gluex/rcdb/python")
 import rcdb
 
 import time
@@ -19,17 +21,20 @@ def main():
     # date and time for 24 hours previous
     beginTime = datetime.datetime.now() - datetime.timedelta(days=1) #days=1
     beginRun = 0
-    CurrentPeriod = "RunPeriod-2018-08"#"RunPeriod-2017-01"
+    CurrentPeriod = "RunPeriod-2019-01"#"RunPeriod-2017-01"
     #print beginTime
     # get first and last runs for the last 24 hours
-    query = r'event_count%3E100000%20and%20@is_2018production' # and time > %s" % beginTime
-    queryfor_rcdb = r'event_count>100000 and @is_2018production' # and time > %s" % beginTime
-    queryshow = "@is_2018production"
-    runs = db.select_runs(queryfor_rcdb)
+    query = r'event_count%3E10000000' # and time > %s" % beginTime
+    queryfor_rcdb = r'event_count>10000000' # and time > %s" % beginTime
+    queryshow = ""
+    runs = db.select_runs(queryfor_rcdb,60000,70000)
+    #print runs
     for run in runs:
-       # print run.number
-       # print run.end_time
-       # print beginTime
+        print run.number
+        print run.end_time
+        print beginTime
+        if beginTime is None:
+            continue
         if run.end_time > beginTime:
             beginRun = run.number
             break
@@ -49,7 +54,7 @@ def main():
     message_file.write( "Occupancy Macros: \n" )
     
     # set list of histograms titles and names in this list
-    hist_names = [["CDC_occupancy","CDC"],["FDC_occupancy","FDC"],["FCAL_occupancy","FCAL"],["BCAL_occupancy","BCAL"],["PS_occupancy","PS"],["RF_TPOL_occupancy","RF & TPOL"],["ST_occupancy","ST"],["TAGGER_occupancy","TAGGER"],["TOF_occupancy","TOF"],["DigiHits_occupancy","Hit Multiplicity"]]
+    hist_names = [["CDC_occupancy","CDC"],["FDC_occupancy","FDC"],["FCAL_occupancy","FCAL"],["BCAL_occupancy","BCAL"],["PS_occupancy","PS"],["RF_TPOL_occupancy","RF & TPOL"],["ST_occupancy","ST"],["TAGGER_occupancy","TAGGER"],["TOF_occupancy","TOF"],["DigiHits_occupancy","Hit Multiplicity"],["DIRC_occupancy","DIRC"],["DIRC_hit","DIRC Hits"],["DIRC_digihit","DIRC DigiHits"],["CCAL_occupancy","CCAL occupancy"],["ccal_cluster_et","CCAL Cluster et"],["ccal_cluster_space","CCAL Cluster Space"],["ccal_comp2","CCAL Comp 2"],["ccal_comp","CCAL Comp"],["ccal_dig_pedestal","CCAL Digi Pedestal"],["ccal_dig_pulse","CCAL Digi Pulse"],["ccal_hit_energy","CCAL Hit Energy"]]
 
     for hist in hist_names:
         message_file.write( "%s: https://halldweb.jlab.org/data_monitoring/Plot_Browser.html?minRunNum=%d&maxRunNum=%d&RunPeriod=%s&Version=rawdata_ver00&Plot=%s&rcdb_query=%s \n\n" % (hist[1], beginRun, endRun, CurrentPeriod, hist[0], query) )
