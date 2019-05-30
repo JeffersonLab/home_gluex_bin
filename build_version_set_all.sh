4a95446305d94f03b9794f587a0c4b590172e03e
Mark Ito <marki@jlab.org>
2019-05-30 19:51:49 +0000

    add container build
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@23987 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- build_version_set_all.sh
+++ build_version_set_all.sh
@@ -8,3 +8,11 @@ do
     env -u SSH_AUTH_SOCK ssh -i ~/.ssh/id_batch $host \
 	/home/gluex/bin/build_version_set.csh $version_xml
 done
+dist=/group/halld/www/halldweb/html/dist
+host=scosg16
+echo launching in singularity container on scosg16
+env -u SSH_AUTH_SOCK ssh -i ~/.ssh/id_batch $host \
+    cat /etc/redhat-release
+env -u SSH_AUTH_SOCK ssh -i ~/.ssh/id_batch $host \
+    singularity exec --bind /group/halld $dist/gluex_centos7.img \
+    /home/gluex/bin/build_version_set.csh $version_xml
