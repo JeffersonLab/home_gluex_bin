a386d24e014354bf7088d859f06953848c470193
Mark Ito <marki@jlab.org>
2019-09-30 13:34:17 +0000

    Ignore kmin, lmin, and lmin from CobremsGeneration.cc, needed for certain preprocessor flag settings.
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@24248 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- nightly_build_message.sh
+++ nightly_build_message.sh
@@ -8,20 +8,28 @@ date > $REPORT_FILE
 echo This and previous messages can be found at https://halldweb.jlab.org/nightly/ >> $REPORT_FILE
 echo ================ ERRORS ================ >> $REPORT_FILE
 grep -e ' Error ' -e 'Command not found' -e 'error: ' -e 'No such file' \
-    $BUILD_DIR/*.log | \
-    grep -v hdview2/trk_mainframe.h \
-    >> $REPORT_FILE
+    $BUILD_DIR/*.log | grep -v hdview2/trk_mainframe.h >> $REPORT_FILE
 echo ================ WARNINGS ============== >> $REPORT_FILE
-grep -e ' warning: ' -e 'Warning: ' -e 'WARNING: ' $BUILD_DIR/*.log | \
-    grep -v hdv_mainframe_Dict.cc | grep -v 'has modification time' | \
-    grep -v 'Clock skew detected' | grep -v 'Obsolete: ' | \
-    grep -v 'AMPTOOLS or CLHEP is not defined' | grep -v dl_routines | \
-    grep -v "variable 'pyk'" | grep -v "variable 'pychge'" | \
-    grep -v "variable 'pycomp'" | grep -v "Nonconforming tab character" | \
-    grep -v "include/Math/Functor.h" \
+grep -e ' warning: ' -e 'Warning: ' -e 'WARNING: ' $BUILD_DIR/*.log \
+    | grep -v hdv_mainframe_Dict.cc \
+    | grep -v 'has modification time' \
+    | grep -v 'Clock skew detected' \
+    | grep -v 'Obsolete: ' \
+    | grep -v 'AMPTOOLS or CLHEP is not defined' \
+    | grep -v dl_routines \
+    | grep -v "variable 'pyk'" \
+    | grep -v "variable 'pychge'" \
+    | grep -v "variable 'pycomp'" \
+    | grep -v "Nonconforming tab character" \
+    | grep -v "include/Math/Functor.h" \
+    | grep -v "\-Wstrict-prototypes" \
+    | grep -v -P "variable \xE2\x80\x98hmin\xE2\x80\x99" \
+    | grep -v -P "variable \xE2\x80\x98kmin\xE2\x80\x99" \
+    | grep -v -P "variable \xE2\x80\x98lmin\xE2\x80\x99" \
     >> $REPORT_FILE
 lines=`wc -l $REPORT_FILE | perl -n -e '@t = split; print $t[0]'`
 echo "number of lines in report file $REPORT_FILE is $lines"
+exit
 if [ $lines -gt 4 ]
 then
     cp -pv $REPORT_FILE $LIST_DIR/message.txt
