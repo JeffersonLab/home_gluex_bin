e8f355dff65901e2d1913fc3289628fc97105e3c
Mark Ito <marki@jlab.org>
2020-03-06 16:56:32 +0000

    Drop ifarm1402, it does exist anymore. Drop printout of /etc/redhad-release in the singularity container.
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@24639 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- build_version_set_all.sh
+++ build_version_set_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 version_xml=$1
-hosts="ifarm1101 ifarm1402 jlabl3 jlabl5"
+hosts="ifarm1402 ifarm1802 jlabl5"
 # loop over hosts
 for host in $hosts
 do
@@ -10,9 +10,7 @@ do
 done
 dist=/group/halld/www/halldweb/html/dist
 host=scosg16
-echo launching in singularity container on scosg16
-env -u SSH_AUTH_SOCK ssh -i ~/.ssh/id_batch $host \
-    cat /etc/redhat-release
+echo launching in singularity container on $host
 env -u SSH_AUTH_SOCK ssh -i ~/.ssh/id_batch $host \
     singularity exec --bind /group/halld $dist/gluex_centos7.img \
     /home/gluex/bin/build_version_set.csh $version_xml
