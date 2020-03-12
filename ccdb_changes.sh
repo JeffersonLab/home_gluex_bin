70493d1237f2151ebb43821199b870a33a6c959b
Mark Ito <marki@jlab.org>
2020-03-12 18:16:51 +0000

    Filter out lines from user = hdsys.
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@24641 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- ccdb_changes.sh
+++ ccdb_changes.sh
@@ -5,7 +5,7 @@ simple_list=$2
 tempfile=/tmp/$2.txt
 rm -f $tempfile
 mysql -t -hhallddb -uccdb_user ccdb -e \
-"select assignments.created, variations.name as variation, directories.name as directory, typeTables.name as type, users.name as author, runMin, runMax, substring(vault, 1, 32) as data, assignments.comment from assignments, constantSets, variations, runRanges, users, typeTables, directories where constantSetId = constantSets.id AND variationId = variations.id and runRanges.id = runRangeId and assignments.authorId = users.id and constantTypeId = typeTables.id and directoryId = directories.id having TIMESTAMPDIFF(HOUR,assignments.created, NOW()) < $hours_since_change order by assignments.created;" > $tempfile
+"select assignments.created, variations.name as variation, directories.name as directory, typeTables.name as type, users.name as author, runMin, runMax, substring(vault, 1, 32) as data, assignments.comment from assignments, constantSets, variations, runRanges, users, typeTables, directories where constantSetId = constantSets.id AND variationId = variations.id and runRanges.id = runRangeId and assignments.authorId = users.id and constantTypeId = typeTables.id and directoryId = directories.id and users.name != 'hdsys' having TIMESTAMPDIFF(HOUR,assignments.created, NOW()) < 4 order by assignments.created;" > $tempfile
 cd $listdir/lists/$simple_list
 cp $tempfile message.txt
 $listdir/scripts/simple_email_list.pl > /dev/null
