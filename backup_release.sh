8f7f4365566f30c8311cd3d4a4576b13f5db72ca
Mark Ito <marki@jlab.org>
2020-06-07 20:44:43 +0000

    script to backup and delete a release
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@24694 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

---
+++ backup_release.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+version_label=$1
+isitok() {
+    prompt=$1
+    while true; do
+	read -p "$prompt" yn
+	case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+	esac
+    done
+}
+script=/tmp/tar_$version_label.sh
+echo version_label = $version_label
+current_dir=`pwd`
+rm -f tar_command.txt
+echo \#\!/bin/bash >$script
+echo pushd /group/halld/Software/builds >>$script
+pushd /group/halld/Software/builds
+find . -maxdepth 3 -type d -name $version_label | awk -F/ '{print "tar zcf /cache/home/gluex/backups/releases/"$4"-"$2".tar.gz "$0}' >>$script
+chmod u+x $script
+echo $script:
+cat $script
+isitok "Is it OK to tar up the release? "
+$script
+echo tar files:
+ls -l /cache/home/gluex/backups/releases/${version_label}*.tar.gz
+isitok "Is it OK to delete the release? "
+find . -maxdepth 3 -type d -name $version_label -print -exec rm -rf {} \;
