f906784ae365335dc3700504917b591409c0aa8f
Mark Ito <marki@jlab.org>
2020-07-19 21:12:52 +0000

    tighten size requirement
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@24743 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- ccdb_sqlite_create.sh
+++ ccdb_sqlite_create.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+size_min=1190000000
 source /group/halld/Software/build_scripts/gluex_env_jlab.sh
 sqlite_file=/group/halld/Software/calib/ccdb_sqlite/ccdb_`date +%F`.sqlite
 rm -f $sqlite_file
 $CCDB_HOME/scripts/mysql2sqlite/mysql2sqlite.sh -hhallddb -uccdb_user ccdb | sqlite3 $sqlite_file | grep -v memory
 size=`stat --format=%s $sqlite_file`
-#echo size = $size
-if [ $size -gt 100000000 ]
+echo size = $size, size_min = $size_min
+if [ $size -gt $size_min ]
     then
     cp -p $sqlite_file /group/halld/www/halldweb/html/dist/ccdb.sqlite.tmp
     cd /group/halld/www/halldweb/html/dist
