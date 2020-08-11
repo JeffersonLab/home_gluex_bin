74354978fe8dd9719db8b23509489be34890115a
Mark Ito <marki@jlab.org>
2020-08-11 15:26:08 +0000

    use ifarm1901, use new devel container with tirpc-devel package
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@24783 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- build_version_set_all.sh
+++ build_version_set_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 version_xml=$1
-hosts="ifarm1802 jlabl5"
+hosts="ifarm1901 jlabl5"
 # loop over hosts
 for host in $hosts
 do
@@ -12,5 +12,5 @@ dist=/group/halld/www/halldweb/html/dist
 host=scosg16
 echo launching in singularity container on $host
 env -u SSH_AUTH_SOCK ssh -i ~/.ssh/id_batch $host \
-    singularity exec --bind /group/halld $dist/gluex_centos7.7.simg \
+    singularity exec --bind /group/halld $dist/gluex_centos7.7-2.simg \
     /home/gluex/bin/build_version_set.csh $version_xml
