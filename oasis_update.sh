6fdf4fee530a304f90bd28ba5b53f4b969707bb4
Mark Ito <marki@jlab.org>
2020-12-09 16:14:55 +0000

    add shipment of CentOS 8 container build
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@24856 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- oasis_update.sh
+++ oasis_update.sh
@@ -11,6 +11,8 @@ rsync_comment[4]="container full build directory"
 rsync_command[4]='rsync -ruvtl --delete --exclude .Linux\* --stats -e '\''ssh -i ~/.ssh/oasis_update_rsa'\'' /group/halld/Software/builds/Linux_CentOS7-x86_64-gcc4.8.5-cntr/recent/ ouser.gluex@oasis-login.opensciencegrid.org:/home/login/ouser.gluex/stage/group/halld/Software/builds/Linux_CentOS7-x86_64-gcc4.8.5-cntr/recent/'
 rsync_comment[5]="halld_versions directory"
 rsync_command[5]='rsync -ruvtl --delete --stats -e '\''ssh -i ~/.ssh/oasis_update_rsa'\'' /group/halld/www/halldweb/html/halld_versions/ ouser.gluex@oasis-login.opensciencegrid.org:/home/login/ouser.gluex/stage/group/halld/www/halldweb/html/halld_versions/'
+rsync_comment[6]="CentOS 8 container build directory"
+rsync_command[6]='rsync -ruvtl --delete --exclude-from=/home/gluex/bin/oasis_exclude_build.txt --stats -e '\''ssh -i ~/.ssh/oasis_update_rsa'\'' /group/halld/Software/builds/Linux_CentOS8-x86_64-gcc8.3.1-cntr/ ouser.gluex@oasis-login.opensciencegrid.org:/home/login/ouser.gluex/stage/group/halld/Software/builds/Linux_CentOS8-x86_64-gcc8.3.1-cntr/'
 files_transferred=false
 for i in "${!rsync_command[@]}";
   do
