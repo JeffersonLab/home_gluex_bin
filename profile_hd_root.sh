6980ba9269682bec54ec6ee6eb6138d062c603fb
Mark Ito <marki@jlab.org>
2021-03-24 16:59:16 +0000

    moved to hd_utilities
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@24928 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- profile_hd_root.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /scratch/gluex
-mkdir -p profile_hd_root
-cd profile_hd_root
-perf record -g hd_root -PEVENTS_TO_KEEP=10000 -PPLUGINS=danarest /cache/halld/RunPeriod-2017-01/rawdata/hd_rawdata_030300_000.evio
-perf report --call-graph fractal --stdio > hd_root_call_graph.txt
