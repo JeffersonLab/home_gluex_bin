e9a8aed9cf8a4bb6a01e0cf37b74ad5654e2c236
Mark Ito <marki@jlab.org>
2021-04-26 13:56:07 +0000

    check only for version set files
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@24932 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- delete_old_nightly.sh
+++ delete_old_nightly.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo delete old nightly builds
 find /u/scratch/gluex/nightly -maxdepth 1 -mindepth 1 -type d | \
-    perl -n -e 'chomp; $dir = $_; $count = `find $dir -type f | wc -l`; chomp $count; print "file count for $dir is $count\n"; if ($count < 1) {print "removing directory $dir\n"; system "rm -rf $dir\n"}'
+    perl -n -e 'chomp; $dir = $_; $count = `find $dir -maxdepth 2 -type f -name \*.xml | wc -l`; chomp $count; print "file count for $dir is $count\n"; if ($count < 1) {print "removing directory $dir\n"; print "rm -rf $dir\n"}'
