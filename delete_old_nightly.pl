a12b78360ff965e3a150822525447368f9d766d5
Mark Ito <marki@jlab.org>
2021-05-06 23:25:54 +0000

    New script to do the deletion of old nightly builds. Formerly coded in the crontab.
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/home/gluex/bin@24936 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

---
+++ delete_old_nightly.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+print "delete old nightly builds\n";
+$find_command = "find /u/scratch/gluex/nightly -maxdepth 1 -mindepth 1 -type d |";
+open (FIND, $find_command);
+while (<FIND>) {
+    chomp;
+    $dir = $_;
+    $count = `find $dir -maxdepth 2 -type f -name \*.xml | wc -l`;
+    chomp $count;
+    if ($count < 2) {
+	print "file count for $dir is $count\n";
+    }
+    if ($count == 0) {
+	print "removing directory $dir\n";
+	system "rm -rf $dir";
+    }
+}
+exit 0;
